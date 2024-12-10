169982b1b529fdef573dcb4b03be82be4f840135
majerv <viktor.majer1@gmail.com>
2024-12-10 07:50:18 +0100

    day10 refactor

--- app/src/main/java/com/vimacodes/aoc/day10/TopographicMap.java
+++ app/src/main/java/com/vimacodes/aoc/day10/TopographicMap.java
@@ -1,79 +1,54 @@
 package com.vimacodes.aoc.day10;
 
 import com.vimacodes.aoc.day6.Pos;
+import com.vimacodes.aoc.utils.Matrix;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
-public record TopographicMap(int rows, int cols, int[][] map, List<Pos> trailHeads) {
+public record TopographicMap(Matrix matrix, List<Pos> trailHeads) {
 
   public static TopographicMap parse(String text) {
-    List<String> lines = text.lines().toList();
-    int rows = lines.size();
-    int cols = lines.get(0).length();
-
-    int[][] map = new int[rows][cols];
+    Matrix matrix = Matrix.parse(text);
     List<Pos> trailHeads = new ArrayList<>();
 
-    System.out.printf("rows: %s, cols: %s\n", rows, cols);
-
-    String line;
-    for (int i = 0; i < rows; i++) {
-      line = lines.get(i);
-      for (int j = 0; j < cols; j++) {
-        int num = Character.getNumericValue(line.charAt(j));
-        //        System.out.print(num);
+    for (int i = 0; i < matrix.rows(); i++) {
+      for (int j = 0; j < matrix.cols(); j++) {
+        int num = matrix.getNum(i, j);
         if (num == 0) {
           trailHeads.add(new Pos(i, j));
         }
-        map[i][j] = num;
       }
-      //      System.out.println();
     }
-    //    System.out.println(trailHeads);
 
-    return new TopographicMap(rows, cols, map, trailHeads);
+    return new TopographicMap(matrix, trailHeads);
   }
 
   public TrailStats stats(Pos start) {
     int allNines = 0;
     var allNinesPos = new HashSet<Pos>();
-    var queue = new LinkedList<Pos>();
 
+    var queue = new LinkedList<Pos>();
     queue.add(start);
 
     while (!queue.isEmpty()) {
       Pos pos = queue.poll();
 
-      int num = getNum(pos);
+      int num = matrix.getNum(pos);
       int next = num + 1;
 
       if (num == 9) {
         allNines++;
         allNinesPos.add(pos);
       } else {
-        if (getNum(pos.up()) == next) queue.add(pos.up());
-        if (getNum(pos.right()) == next) queue.add(pos.right());
-        if (getNum(pos.down()) == next) queue.add(pos.down());
-        if (getNum(pos.left()) == next) queue.add(pos.left());
+        if (matrix.isEqualTo(pos.up(), next)) queue.add(pos.up());
+        if (matrix.isEqualTo(pos.right(), next)) queue.add(pos.right());
+        if (matrix.isEqualTo(pos.down(), next)) queue.add(pos.down());
+        if (matrix.isEqualTo(pos.left(), next)) queue.add(pos.left());
       }
     }
 
-    TrailStats trailStats = new TrailStats(start, allNinesPos.size(), allNines);
-    //    System.out.println("Trailhead stats: " + trailStats);
-    return trailStats;
-  }
-
-  private int getNum(Pos pos) {
-    return validPos(pos) ? map[pos.i()][pos.j()] : -1;
-  }
-
-  private boolean validPos(Pos pos) {
-    return validPos(pos.i(), pos.j());
-  }
-
-  private boolean validPos(int i, int j) {
-    return 0 <= i && i < rows && 0 <= j && j < cols;
+    return new TrailStats(start, allNinesPos.size(), allNines);
   }
 }
---
+++ app/src/main/java/com/vimacodes/aoc/utils/Matrix.java
@@ -0,0 +1,46 @@
+package com.vimacodes.aoc.utils;
+
+import com.vimacodes.aoc.day6.Pos;
+import java.util.List;
+
+public record Matrix(int rows, int cols, char[][] map) {
+
+  public static Matrix parse(String text) {
+    List<String> lines = text.lines().toList();
+    int rows = lines.size();
+    int cols = lines.get(0).length();
+
+    char[][] map = new char[rows][cols];
+
+    String line;
+    for (int i = 0; i < rows; i++) {
+      line = lines.get(i);
+      for (int j = 0; j < cols; j++) {
+        char c = line.charAt(j);
+        map[i][j] = c;
+      }
+    }
+
+    return new Matrix(rows, cols, map);
+  }
+
+  public int getNum(int i, int j) {
+    return Character.getNumericValue(map[i][j]);
+  }
+
+  public int getNum(Pos pos) {
+    return getNum(pos.i(), pos.j());
+  }
+
+  public boolean isEqualTo(Pos pos, int comparedTo) {
+    return validPos(pos) && getNum(pos) == comparedTo;
+  }
+
+  public boolean validPos(Pos pos) {
+    return validPos(pos.i(), pos.j());
+  }
+
+  public boolean validPos(int i, int j) {
+    return 0 <= i && i < rows && 0 <= j && j < cols;
+  }
+}
